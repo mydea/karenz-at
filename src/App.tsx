255dde6ee689debb452b7919842f5cde5042f657
Francesco Novy <francesco.novy@sentry.io>
2026-03-23 10:48:13 +0100

    scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import SettingsPage from './pages/SettingsPage';
@@ -6,17 +7,30 @@ import TimelinePage from './pages/TimelinePage';
 import CalculatorPage from './pages/CalculatorPage';
 import FaqPage from './pages/FaqPage';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="einstellungen" element={<SettingsPage />} />
-        <Route path="zeitplan" element={<TimelinePage />} />
-        <Route path="rechner" element={<CalculatorPage />} />
-        <Route path="faq" element={<FaqPage />} />
-      </Route>
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="einstellungen" element={<SettingsPage />} />
+          <Route path="zeitplan" element={<TimelinePage />} />
+          <Route path="rechner" element={<CalculatorPage />} />
+          <Route path="faq" element={<FaqPage />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
